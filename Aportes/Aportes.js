1ba7b2449cd2c2158124532aa1c99f9fd269d5fb
Gonzalo_Callejas <gonzalocallejasrojas@gmail.com>
2023-10-09 20:06:39 +0200

    version post entrega

--- Aportes/Aportes.js
+++ Aportes/Aportes.js
@@ -4,7 +4,7 @@ const apellidos = document.getElementById("apellidos");
 
 const enviarAporte = () => {
     console.log(cuerpo.value);
-    window.location=`mailto:gonzalocallejasrojas@gmail.com?subject=Aporte - ${tipo}&body=Buenas, soy ${nombre.value} ${apellidos.value} %0D%0A Le escribo para notificar el siguiente problema: ${cuerpo.value} ` ;
+    window.location=`mailto:gonzalocallejasrojas@gmail.com?subject=Aporte - ${tipo}&body=Buenas, soy ${nombre.value} ${apellidos.value} %0D%0A Le escribo para notificar el siguiente problema, ${cuerpo.value} ` ;
 }
 let tipo = 'Otro';
 
